3722f126f73bbed03f72a69249d199dfcd9b2931
Thuan Nguyen <thuannv06022001@gmail.com>
2025-05-08 16:45:24 +0700

    update website

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+const allowedOrigins = ["http://localhost:5173"];
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser);
+app.use(cors({ origin: allowedOrigins, credential: true }));
+
+app.get("/", (req, res) => res.send("API is Working"));
+app.listen(port, () => {
+  console.log(`Server in running on http://localhost:${port}`);
+});
